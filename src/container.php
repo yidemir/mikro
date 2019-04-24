52296a0a68c12e90ee5275c9d1dbfcdc95bba11d
yidemir <demiriy@gmail.com>
2019-04-24 10:24:45 +0300

    Container modülü oluşturuldu

---
+++ src/container.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace container;
+
+use Closure;
+use Exception;
+
+function collection(array $items = []): array
+{
+    static $collection;
+
+    if ($collection === null) {
+        $collection = [];
+    }
+
+    if ($items !== []) {
+        $collection = \array_merge($collection, $items);
+    }
+
+    return $collection;
+}
+
+function has(string $name): bool
+{
+    return \array_key_exists($name, collection());
+}
+
+function get(string $name, array $args = [])
+{
+    $items = collection();
+
+    if (\array_key_exists($name, $items)) {
+        if ($items[$name] instanceof Closure) {
+            return \call_user_func_array($items[$name], $args);
+        } else {
+            return $items[$name];
+        }
+    }
+
+    throw new Exception(\sprintf('Öğe mevcut değil: "%s"', $name));
+}
+
+function set(string $name, $data)
+{
+    collection([$name => $data]);
+}
+
+function singleton(string $name, Closure $callback)
+{
+    collection([$name => function() use ($callback) {
+        static $object;
+
+        if ($object === null) {
+            $object = $callback();
+        }
+
+        return $object;
+    }]);
+}
