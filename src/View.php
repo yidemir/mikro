68413bf1995e26ec28f7dad499ffb70d0311b80f
Yilmaz Demir <demiriy@gmail.com>
2022-03-20 19:16:15 +0300

    View template parser feature and push method added

--- src/View.php
+++ src/View.php
@@ -37,10 +37,10 @@ namespace View
         \ob_start();
 
         if (! empty($data)) {
-            \extract($data);
+            \extract($data, \EXTR_SKIP);
         }
 
-        require $path;
+        require cache($path);
 
         return \ltrim((string) \ob_get_clean());
     }
@@ -83,7 +83,7 @@ namespace View
      * View\stop();
      * ```
      */
-    function stop(): void
+    function stop(bool $push = false): void
     {
         global $mikro;
 
@@ -91,7 +91,22 @@ namespace View
             throw new ViewException('View block not started');
         }
 
-        set(\array_pop($mikro[ACTUAL_BLOCKS]), \ob_get_clean());
+        $actual = \array_pop($mikro[ACTUAL_BLOCKS]);
+
+        set($actual, $push ? (get($actual) ?? '') . \ob_get_clean() : \ob_get_clean());
+    }
+
+    /**
+     * Turn off output buffering and push/append the view block data
+     *
+     * {@inheritDoc} **Example:**
+     * ```php
+     * View\stop();
+     * ```
+     */
+    function push(): void
+    {
+        stop(true);
     }
 
     /**
@@ -130,6 +145,85 @@ namespace View
         return $mikro[BLOCKS][$name] ?? ($default instanceof \Closure ? $default() : $default);
     }
 
+    function cache(string $path): string
+    {
+        global $mikro;
+
+        $cachePath = $mikro[CACHE_PATH] ?? $mikro[PATH] . '/cache';
+
+        if (! \file_exists($cachePath)) {
+            \mkdir($cachePath, 0744);
+        }
+
+        $cachedPath = $cachePath . '/' . \md5($path) . '.php';
+
+        if (! \is_file($cachedPath) || \filemtime($cachedPath) < \filemtime($path)) {
+            $contents = template(\file_get_contents($path));
+            \file_put_contents($cachedPath, $contents);
+        }
+
+        return $cachedPath;
+    }
+
+    function template(string $data): string
+    {
+        global $mikro;
+
+        if (
+            isset($mikro[DELIMITER]) &&
+            \is_array($mikro[DELIMITER]) &&
+            \count($mikro[DELIMITER]) === 2
+        ) {
+            [$start, $end] = $mikro[DELIMITER];
+        } else {
+            [$start, $end] = ['@', ';'];
+        }
+
+        $data = \preg_replace_callback(
+            "~(?!<\?php.*){$start}=(.+?[^\\\;]){$end}(?!.*\?>)~m",
+            function (array $matches) use ($end) {
+                $data = \str_replace('\\' . $end, $end, $matches[1]);
+
+                return '<?php echo \View\e(' . $data . ') ?>';
+            },
+            $data
+        );
+
+        return \preg_replace_callback(
+            "~(?!<\?php.*){$start}(.+?[^\\\;]){$end}(?!.*\?>)~m",
+            function ($matches) use ($mikro, $end) {
+                $data = \str_replace('\\' . $end, $end, $matches[1]);
+
+                foreach (($mikro[METHODS] ?? []) as $method => $callback) {
+                    if (\str_starts_with($data, $method . ' ') || $method === $data) {
+                        $data = \preg_replace('/' . $method . ' /', '', $data, 1);
+
+                        return $callback(\str_replace('\\' . $end, $end, $data));
+                    }
+                }
+
+                return '<?php ' . $data . ' ?>';
+            },
+            $data
+        );
+    }
+
+    function method(string $method, callable $callback): void
+    {
+        global $mikro;
+
+        $mikro[METHODS][$method] = $callback;
+    }
+
+    function clear(): void
+    {
+        global $mikro;
+
+        foreach (\glob($mikro[CACHE_PATH] ?? $mikro[PATH] . '/cache/*.php') as $file) {
+            \unlink($file);
+        }
+    }
+
     /**
      * View path constant
      *
@@ -152,6 +246,26 @@ namespace View
     const EXTENSION = 'View\EXTENSION';
 
     /**
+     * View template cache path
+     *
+     * {@inheritDoc} **Example:**
+     * ```php
+     * $mikro[View\CACHE_PATH] = __DIR__ . '/views/cache';
+     * ```
+     */
+    const CACHE_PATH = 'View\CACHE_PATH';
+
+    /**
+     * View renderer delimiter
+     *
+     * {@inheritDoc} **Example:**
+     * ```php
+     * $mikro[View\DELIMITER] = ['@', ';'];
+     * ```
+     */
+    const DELIMITER = 'View\DELIMITER';
+
+    /**
      * View actual block constant
      *
      * @internal
@@ -164,4 +278,11 @@ namespace View
      * @internal
      */
     const BLOCKS = 'View\BLOCKS';
+
+    /**
+     * View renderer methods
+     *
+     * @internal
+     */
+    const METHODS = 'View\METHODS';
 };
